60fbe4a60e5f1097d87bb97367815a70d08f50dc
David Méndez <davidgmendezn@gmail.com>
2023-02-06 17:22:50 -0500

    checkpoint

---
+++ src/test/java/guru/sfg/brewery/web/controllers/indexControllerIT.java
@@ -0,0 +1,18 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class indexControllerIT extends BaseIT {
+
+    @Test
+    void testGetIndexSlash() throws Exception {
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+
+    }
+
+}
